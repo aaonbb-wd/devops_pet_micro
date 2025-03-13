bc22c2343d6f015a4daad067ac874a11a0b8e172
MykeKoby <aaonbb@gmail.com>
2025-03-13 13:28:19 +0200

    Complete WordPress deployment with Ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,5 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.wordpress.id
-}
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.wordpress.public_ip
-}
-
-output "ssh_command" {
-  description = "SSH command to connect to the instance"
-  value       = "ssh -i ${var.ssh_key_name}.pem ec2-user@${aws_instance.wordpress.public_ip}"
 }
